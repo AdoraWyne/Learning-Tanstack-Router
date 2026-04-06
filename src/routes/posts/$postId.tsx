2902d9825522b87b06d25271ce5d9d5820f427e7
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-06 23:17:20 +1000

    Add note about: - calling API to fetch data will be in loader async function. - then return the data as part of the loader object. - then you can access the data from Route.useLoaderData()

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -4,6 +4,10 @@ export const Route = createFileRoute("/posts/$postId")({
   component: RouteComponent,
   // loader is to get the data that the component needs before component uses and renders it
   loader: async ({ params }) => {
+    // fetching the data that this component needs here
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    // then you can return the data here
     return {
       postId: params.postId,
     };
@@ -15,25 +19,3 @@ function RouteComponent() {
 
   return <div>Hello {postId}!</div>;
 }
-
-// import { createFileRoute } from "@tanstack/react-router";
-
-// export const Route = createFileRoute("/posts/$postId")({
-//   component: RouteComponent,
-//   // loader is to get the data that the component needs before component uses and renders it
-//   loader: async ({ params }) => {
-//     // call the data that this component needs here
-//     await new Promise((resolve) => setTimeout(resolve, 5000));
-
-//     return {
-//       postId: params.postId,
-//     };
-//   },
-//   pendingComponent: () => <div>Loading...</div>,
-// });
-
-// function RouteComponent() {
-//   const { postId } = Route.useLoaderData();
-
-//   return <div>Hello {postId}!</div>;
-// }
