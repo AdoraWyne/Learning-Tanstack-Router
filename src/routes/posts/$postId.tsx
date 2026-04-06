9b13d559b4e3b693489454a1ef189072d1a19ca2
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-06 23:14:31 +1000

    - Apply loader async function to access to $postId - Then use Route.useLoaderData() to access the object from the loader

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -2,8 +2,38 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/posts/$postId")({
   component: RouteComponent,
+  // loader is to get the data that the component needs before component uses and renders it
+  loader: async ({ params }) => {
+    return {
+      postId: params.postId,
+    };
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/posts/$postId"!</div>;
+  const { postId } = Route.useLoaderData();
+
+  return <div>Hello {postId}!</div>;
 }
+
+// import { createFileRoute } from "@tanstack/react-router";
+
+// export const Route = createFileRoute("/posts/$postId")({
+//   component: RouteComponent,
+//   // loader is to get the data that the component needs before component uses and renders it
+//   loader: async ({ params }) => {
+//     // call the data that this component needs here
+//     await new Promise((resolve) => setTimeout(resolve, 5000));
+
+//     return {
+//       postId: params.postId,
+//     };
+//   },
+//   pendingComponent: () => <div>Loading...</div>,
+// });
+
+// function RouteComponent() {
+//   const { postId } = Route.useLoaderData();
+
+//   return <div>Hello {postId}!</div>;
+// }
