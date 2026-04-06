35bc48a4de9a2dcaccd3090af63f2341dbdf4d66
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-06 22:58:42 +1000

    - Create posts index page - Create dynamic post[id] page with `$` as part of the file name. - routeTree.gen.ts auto update.

---
+++ src/routes/posts/index.tsx
@@ -0,0 +1,21 @@
+import { createFileRoute, Link } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/posts/")({
+  component: Posts,
+});
+
+function Posts() {
+  const posts = ["post1", "post2", "post3"];
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post}>
+          <Link to="/posts/$postId" params={{ postId: post }}>
+            {post}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
